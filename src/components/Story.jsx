84ab59342f8852a1f4c142732afd520f9ce03bfd
Great Ochuko <greatochuko@gmail.com>
2023-10-17 19:25:24 +0100

    finished add story styling

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -4,17 +4,35 @@ import PropTypes from "prop-types";
 
 export default function Story({ story }) {
   const [isHover, setIsHover] = useState(false);
+  if (story) {
+    return (
+      <div
+        className={`${styles.story} ${isHover ? styles.hover : ""}`}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <img
+          src={`http://localhost:5000/${story.imgUrl} `}
+          alt={story.imgUrl}
+        />
+        <div className={styles.creatorDetails}>
+          <img src={story.user.imgUrl} alt={story.user.username} />
+          <h4>@{story.user.username}</h4>
+        </div>
+        <div className={styles.overlay}></div>
+      </div>
+    );
+  }
   return (
     <div
-      className={`${styles.story} ${isHover ? styles.hover : ""}`}
+      className={`${styles.story} ${styles.add} ${isHover ? styles.hover : ""}`}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
     >
-      <img src={`http://localhost:5000/${story.imgUrl} `} alt={story.imgUrl} />
-      <div className={styles.creatorDetails}>
-        <img src={story.user.imgUrl} alt={story.user.username} />
-        <h4>@{story.user.username}</h4>
-      </div>
+      <button className={styles.addStory}>
+        <span>+</span>
+        <p>Add Story</p>
+      </button>
       <div className={styles.overlay}></div>
     </div>
   );
