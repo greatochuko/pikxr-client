942eda84dc4630642edf3e0b67a56a1f73c52164
Great Ochuko <greatochuko@gmail.com>
2023-10-18 10:24:19 +0100

    added option button to post component and added fullname to creator component

--- src/components/Creator.jsx
+++ src/components/Creator.jsx
@@ -1,18 +1,34 @@
 import styles from "./Creator.module.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-
-export default function Creator({ post, className }) {
+export default function Creator({ post, className, children }) {
   return (
     <div className={styles.creator + " " + className}>
-      <img src={post.creator.imgUrl} alt={post.creator.username} />
-      <h4>@{post.creator.username}</h4>
-      <div className={styles.options}></div>
+      <Link>
+        <img src={post.creator.imgUrl} alt={post.creator.username} />
+        <div className={styles.text}>
+          <h4>{post.creator.fullname}</h4>
+          <p>@{post.creator.username}</p>
+        </div>
+      </Link>
+      <button className={styles.options}>
+        <i className="fa-solid fa-ellipsis-vertical"></i>
+        <ul className={styles.optionList}>
+          <li>
+            <Link> Delete Post</Link>
+          </li>
+          <li>
+            <Link>Goto Post</Link>
+          </li>
+        </ul>
+      </button>
     </div>
   );
 }
 
 Creator.propTypes = {
   post: PropTypes.object,
-  className: PropTypes.string
-}
+  className: PropTypes.string,
+  children: PropTypes.element,
+};
