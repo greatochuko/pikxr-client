47be69667ea21d6514538961b66704ed3ce2f9aa
Great Ochuko <greatochuko@gmail.com>
2023-10-19 19:21:25 +0100

    added open post view modal functionality

--- src/components/MasonryGridPost.jsx
+++ src/components/MasonryGridPost.jsx
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux";
 import styles from "./MasonryGridPost.module.css";
+import { setModalPost, togglemodal } from "../slice/postSlice";
 
 export default function MasonryGridPost({ post }) {
+  const dispatch = useDispatch();
+
+  function openPostViewModal() {
+    dispatch(setModalPost(post));
+    dispatch(togglemodal("viewPost"));
+  }
+
   return (
-    <div className={styles.post}>
+    <div className={styles.post} onClick={openPostViewModal}>
       <img src={`http://localhost:5000/${post.imageUrl}`} alt="" />
       <div className={styles.overlay}>
         <span>
