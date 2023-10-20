852fa633072a64164512ef2462ccf80ddf3e7c6b
Great Ochuko <greatochuko@gmail.com>
2023-10-20 23:51:40 +0100

    finished styling profile page

---
+++ src/components/ProfilePostGrid.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import styles from "./ProfilePostGrid.module.css";
+import ProfilePost from "./ProfilePost";
+import propTypes from "prop-types";
+
+export default function ProfilePostGrid({ type }) {
+  const { posts } = useSelector((state) => state.post);
+  const { user } = useSelector((state) => state.user);
+  let filteredPosts = posts;
+
+  if (type === "posts") {
+    filteredPosts = posts.filter((p) => p.creator._id === user._id);
+  } else if (type === "liked") {
+    filteredPosts = posts.filter((p) => user.likedPosts.includes(p._id));
+  } else if (type === "saved") {
+    filteredPosts = posts.filter((p) => user.savedPosts.includes(p._id));
+  }
+
+  return (
+    <div className={styles.postGrid}>
+      {filteredPosts.map((post) => (
+        <ProfilePost post={post} key={post._id} />
+      ))}
+    </div>
+  );
+}
+
+ProfilePostGrid.propTypes = {
+  type: propTypes.string,
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,73 @@
+import { useSelector } from "react-redux";
+import styles from "./Profile.module.css";
+import { useState } from "react";
+import ProfilePostGrid from "../components/ProfilePostGrid";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  const { user } = useSelector((state) => state.user);
+  const [activeTab, setActiveTab] = useState("posts");
+
+  return (
+    <main className={styles.profile}>
+      <div className={styles.profileImages}>
+        <img
+          src="cover-photo.png"
+          alt="cover photo"
+          className={styles.coverPhoto}
+        />
+        <img
+          src={user.imageUrl}
+          alt="profile picture"
+          className={styles.profileImage}
+        />
+      </div>
+      <div className={styles.main}>
+        <div className={styles.userInfo}>
+          <h3 className={styles.name}>{user.fullname}</h3>
+          <p className={styles.username}>@{user.username}</p>
+          <p className={styles.email}>@{user.email}</p>
+          <ul className={styles.userStats}>
+            <li>
+              <span>46</span>Posts
+            </li>
+            <li>
+              <span>2.8k</span>Followers
+            </li>
+            <li>
+              <span>125</span>Following
+            </li>
+          </ul>
+          <p className={styles.about}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error,
+            minima! Sint officia voluptas aperiam commodi?
+          </p>
+        </div>
+        <div className={styles.posts}>
+          <div className={styles.header}>
+            <ul>
+              <li
+                onClick={() => setActiveTab("posts")}
+                className={activeTab === "posts" ? styles.active : ""}
+              >
+                Posts
+              </li>
+              <li
+                onClick={() => setActiveTab("liked")}
+                className={activeTab === "liked" ? styles.active : ""}
+              >
+                Liked
+              </li>
+              <li
+                onClick={() => setActiveTab("saved")}
+                className={activeTab === "saved" ? styles.active : ""}
+              >
+                Saved
+              </li>
+            </ul>
+          </div>
+          <ProfilePostGrid type={activeTab} />
+        </div>
+      </div>
+    </main>
+  );
 }
